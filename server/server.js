707dd9d6ec7703635ef9e663ccb19d33c3ffc84e
onur-yildiz <onur.yildizcp13@hotmail.com>
2018-08-02 12:58:24 +0300

    cors

--- server/server.js
+++ server/server.js
@@ -14,6 +14,14 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 app.get('/requests', authenticate, async (req, res) => {
   const requests = await Request.find({ _owner: req.user._id });
